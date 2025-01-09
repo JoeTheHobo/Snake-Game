5cc4cce6e5fa2475cc373637a3459d63d04218cf
JoeTheHobo <36902814+JoeTheHobo@users.noreply.github.com>
2025-01-08 17:43:13 -0700

    Add files via upload

--- functions.js
+++ functions.js
@@ -12,8 +12,35 @@ function getItem(name) {
 function spawn(name) { 
     for (let i = 0; i < items.length; i++) {
         if (items[i].name == name) {
-            let x = rnd(gridX)-1;
-            let y = rnd(gridY)-1
+
+            let counter = 0;
+            let foundSpot = false;
+            let x,y;
+            while (foundSpot == false) {
+
+                x = rnd(gridX)-1;
+                y = rnd(gridY)-1
+
+                if (map[y][x].name == "air") {
+                    foundSpot = true;
+                }
+                counter++;
+                if (counter > (gridX * gridY) ) {
+                    foundSpot = "couldn't find any";
+                }
+            }
+
+            if (foundSpot == "couldn't find any") {
+                findingAnySpot: for (let k = 0; k < gridY; k++) {
+                    for (let j = 0; j < gridX; j++) {
+                        if (map[k][j].name == "air") {
+                            x = j;
+                            y = k;
+                            break findingAnySpot;
+                        }
+                    }
+                }
+            }
 
             map[y][x] = items[i];
             updateCells.push({
