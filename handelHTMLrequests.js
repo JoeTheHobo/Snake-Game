ca050887f8bd675039c6566a9d8214d1c47009b9
JoeTheHobo <johnjonesma@gmail.com>
2025-02-01 09:43:42 -0700

    changes

--- handelHTMLrequests.js
+++ handelHTMLrequests.js
@@ -1,47 +1,58 @@
 $("local_button_playGame").on("click",function() {
-    if (activePlayerCount.length == 0) {
-        makePopUp([
-            {type: "title",color: "red",text: "Select Snake(s) Before Playing Game"},
-            {type: "button",close: true,cursor: "url('./img/pointer.cur'), auto", width: "100%",background: "green",text:"Return"},
-        ],{
-            exit: {
-                cursor: "url('./img/pointer.cur'), auto",
-            },
-            id: "warning",
-
-        })
-    } else {
-        $(".game_canvas").classRemove("singlePlayerCanvas");
-        $(".game_canvas").css({
-            left: 0,
-            top: 0,
-            right: 0,
-            bottom: 0,
-            margin: "auto",
-        })
-        cameraFollowPlayer = false;
-        startGame();
-        $(".button_mapEditorHolder").hide();
+    let minPlayers = currentBoard.minPlayers;
+    let maxPlayers = currentBoard.maxPlayers;
+    let playerCount = activePlayerCount.length;
+    if (playerCount < minPlayers) {
+        warningPopup("At Least " + minPlayers + ` Snake${minPlayers === 1 ? "" : "s"} Required`);
+        return;
     }
+    if (playerCount > maxPlayers) {
+        warningPopup("No More Than " + maxPlayers + ` Snake${maxPlayers === 1 ? "" : "s"} Allowed`);
+        return;
+    }
+
+    $(".game_canvas").classRemove("singlePlayerCanvas");
+    $(".game_canvas").css({
+        left: 0,
+        top: 0,
+        right: 0,
+        bottom: 0,
+        margin: "auto",
+    })
+    cameraFollowPlayer = false;
+    startGame();
+    $(".button_mapEditorHolder").hide();
+    
 })
-$("local_button_playSolo").on("click",function() {
-    if (activePlayerCount.length == 0) {
-        makePopUp([
-            {type: "title",color: "red",text: "Select A Snake Before Playing Game"},
-            {type: "button",close: true,cursor: "url('./img/pointer.cur'), auto", width: "100%",background: "green",text:"Return"},
-        ],{
-            exit: {
-                cursor: "url('./img/pointer.cur'), auto",
-            },
-            id: "warning",
+function warningPopup(text) {
+    makePopUp([
+        {type: "title",color: "red",text: text},
+        {type: "button",close: true,cursor: "url('./img/pointer.cur'), auto", width: "100%",background: "green",text:"Return"},
+    ],{
+        exit: {
+            cursor: "url('./img/pointer.cur'), auto",
+        },
+        id: "warning",
 
-        })
-    } else {
-        $(".game_canvas").classAdd("singlePlayerCanvas");
-        cameraFollowPlayer = true;
-        startGame();
-        $(".button_mapEditorHolder").hide();
+    })
+}
+$("local_button_playSolo").on("click",function() {
+    let minPlayers = currentBoard.minPlayers;
+    let playerCount = activePlayerCount.length;
+    if (minPlayers > 1) {
+        warningPopup(`Solo Mode Not Allowed On This Board`);
+        return;
     }
+    if (playerCount == 0) {
+        warningPopup(`Select A Snake Before Playing Game`);
+        return;
+    }
+    
+    $(".game_canvas").classAdd("singlePlayerCanvas");
+    cameraFollowPlayer = true;
+    startGame();
+    $(".button_mapEditorHolder").hide();
+    
 })
 $("button_mainMenu").on("click",function() {
     setScene("newMenu")
