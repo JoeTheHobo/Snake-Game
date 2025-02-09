5f2f09755f872a316161ccdb2038b8d88688c514
JoeTheHobo <johnjonesma@gmail.com>
2025-02-09 01:01:35 -0700

    asdf

--- public/main.js
+++ public/main.js
@@ -1569,7 +1569,7 @@ function serverGameLoop() {
     deleteSnakeCells();
     renderPlayers();
 
-    console.log(Math.abs(localUpdated/2-Date.now()-localTimeStamp),Math.abs(localUpdated/2-Date.now()-localTimeStamp)<10);
+    console.log(Math.abs((localUpdated/2)-(Date.now()-localTimeStamp))<10);
 
     if (!gameEnd && !killSwitch) setTimeout(() => serverGameLoop(), 1000/60);;//requestAnimationFrame(gameLoop);
 }
