639a3f3f99ff797156a92fe8a7971182e3df5888
JoeTheHobo <johnjonesma@gmail.com>
2025-02-08 21:30:34 -0700

    asdf

--- app.js
+++ app.js
@@ -548,7 +548,6 @@ io.on('connection', (socket) => {
             server_movePlayers(this);
             
             if (onlineAccounts[socket.id]) {
-                console.log(onlineAccounts[socket.id].player.pos);
                 io.emit("updatedLocalAccount",{
                     id: socket.id,
                     isInGame: true,
@@ -559,7 +558,7 @@ io.on('connection', (socket) => {
             
 
             if (!this.gameEnd) {
-                setTimeout(() => this.gameLoop(), 1000/*Math.max(0, (1000/60) - (Date.now() - timestamp))*/);
+                setTimeout(() => this.gameLoop(), 1000/60/*Math.max(0, (1000/60) - (Date.now() - timestamp))*/);
             } else {
                 this.isActiveGame = false;
 
--- public/index.js
+++ public/index.js
@@ -173,7 +173,6 @@ socket.on("updatedLocalAccount",(obj) => {
         localAccount.activePlayers[i].canvas = canvasList[i];
     }
     let playerCanvas = localAccount.player.canvas;
-    console.log(obj.player.pos)
     localAccount.player = obj.player;
     localAccount.player.canvas = playerCanvas;
     localAccount.updateSnakeCells = updateSnakeCells.concat(obj.lobby.updateSnakeCells);
