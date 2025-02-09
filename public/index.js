b73186b5e5d3ebeba77a1b26fe35e7519e5270b9
JoeTheHobo <johnjonesma@gmail.com>
2025-02-09 14:57:47 -0700

    asdf

--- public/index.js
+++ public/index.js
@@ -171,7 +171,7 @@ socket.on("updatePositions",(obj) => {
     deltaTime = obj.deltaTime;
 
     updateSnakeCells = updateSnakeCells.concat(obj.updateSnakeCells);
-    updateSnakeCells.push({x: activePlayers[0].pos.x,y:activePlayers[1].pos.y})
+    updateSnakeCells.push({x: activePlayers[0].pos.x,y:activePlayers[0].pos.y})
     activePlayers[0].pos = obj.activePlayers[0].pos;
 
     if (obj.dontSend) return;
