80d3a69ca09187e91278f3ae3df724da84097125
JoeTheHobo <johnjonesma@gmail.com>
2025-02-09 12:34:09 -0700

    asdf

--- public/main.js
+++ public/main.js
@@ -1572,7 +1572,7 @@ function serverGameLoop() {
     movePlayers();
     
 
-    if (!gameEnd && !killSwitch) setTimeout(() => serverGameLoop(), 1000/60);;//requestAnimationFrame(gameLoop);
+    if (!gameEnd && !killSwitch) setTimeout(() => serverGameLoop(), 1000/50);;//requestAnimationFrame(gameLoop);
 }
 function specialItemManager() {
     if (gameType == "server") return;
