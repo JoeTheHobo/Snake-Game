7bdf4a17ad94420b2e0c6d0d7053119a34304e08
JoeTheHobo <johnjonesma@gmail.com>
2025-02-08 23:12:11 -0700

    asdf

--- public/main.js
+++ public/main.js
@@ -1560,7 +1560,7 @@ function gameLoop(timestamp) {
     renderEmotes();
 
     updateProduction();
-    if (!gameEnd && !gamePaused && !killSwitch) setTimeout(() => gameLoop(), Math.max(0, (1000/60) - (Date.now() - timestamp)));;//requestAnimationFrame(gameLoop);
+    if (!gameEnd && !gamePaused && !killSwitch) setTimeout(() => gameLoop(), Math.max(0, (1000/30) - (Date.now() - timestamp)));;//requestAnimationFrame(gameLoop);
 }
 
 function specialItemManager()
