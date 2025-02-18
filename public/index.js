d860c688a9cce22f872883c2a44deebb04b53557
JoeTheHobo <johnjonesma@gmail.com>
2025-02-18 16:05:14 -0700

    adsf

--- public/index.js
+++ public/index.js
@@ -98,7 +98,7 @@ socket.on("startingGame", (lobby) => {
     generatePlayerCards(activePlayers);
     serverGameLoop();
 
-    let color = _color(getAverageCanvasColor(canvas_tiles)).darken(10).color;
+    let color = _color(getAverageCanvasColor(canvas_tiles)).darken(10).ogColor;
     console.log(color)
     document.body.style.background = color;
     
