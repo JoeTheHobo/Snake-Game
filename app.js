40518dac0a636dc209823f5a7c7266a1f11e5899
JoeTheHobo <johnjonesma@gmail.com>
2025-02-17 23:08:51 -0700

    asdf

--- app.js
+++ app.js
@@ -1824,7 +1824,7 @@ function server_movePlayers(lobby) {
             }
             if (player.tail.length == 1) snakeMapSetSibling(lobby,player.index,player.tail[0].y,player.tail[0].x,player.pos.y,player.pos.x);
 
-            if (lobby.snakeMap[player.tail[player.tail.length-1].y][player.tail[player.tail.length-1].x].length > 2) console.log(2,lobby.snakeMap[player.tail[player.tail.length-1].y][player.tail[player.tail.length-1].x])
+            if (player.tail.length > 0) if (lobby.snakeMap[player.tail[player.tail.length-1].y][player.tail[player.tail.length-1].x].length > 2) console.log(2,lobby.snakeMap[player.tail[player.tail.length-1].y][player.tail[player.tail.length-1].x])
 
             lobby.updateSnakeCells.push(lobby.snakeMap[playerY][playerX]);
             lobby.updateSnakeCells.push(lobby.snakeMap[player.pos.y][player.pos.x]);
