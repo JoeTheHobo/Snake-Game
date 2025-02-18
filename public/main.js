5b064f9919f259eb2c360198332ab365fa011ab3
JoeTheHobo <johnjonesma@gmail.com>
2025-02-17 22:22:53 -0700

    asfd

--- public/main.js
+++ public/main.js
@@ -210,10 +210,7 @@ function server_renderPlayers() {
     
                 if (obj.type == "tail") {
                     image = player.canvas.tail;
-                    if (active.includes("right")) direction = "left"; //
-                    if (active.includes("left")) direction = "right"; //*
-                    if (active.includes("bottom")) direction = "up"; //
-                    if (active.includes("top")) direction = "down"; //*
+                    direction = active[0]; 
                 } else {
                     if (active.includes("left") && active.includes("right")) {
                         image = player.canvas.body;
