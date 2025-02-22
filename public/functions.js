bd1e0f5bdde77991ee172cafc82fc11056270eca
JoeTheHobo <johnjonesma@gmail.com>
2025-02-22 14:32:17 -0700

    asdf

--- public/functions.js
+++ public/functions.js
@@ -1290,7 +1290,7 @@ function updateGameScene(player) {
         let item = player.items[i];
 
         if (player.selectingItem === i) holder.$(".game_cc_pi_item_wallIMG2").css({
-            filter: "blur(30px) saturate(5)",
+            filter: "hue-rotate(90deg)",
         })
 
         if (item == "empty") {
