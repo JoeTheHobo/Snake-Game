8e0ae44fe3b89d19250f59c1c48432e015968e9f
JoeTheHobo <johnjonesma@gmail.com>
2025-02-23 16:01:44 -0700

    asdf

--- app.js
+++ app.js
@@ -1024,6 +1024,7 @@ io.on('connection', (socket) => {
 
         lobby.readyPlayers.push(socket.id);
 
+        console.log(lobby.readyPlayers,lobby.inGamePlayers);
         if (lobby.readyPlayers.length === lobby.inGamePlayers) {
             io.emit("preparingGame",lobby.id);
             setTimeout(function() {
