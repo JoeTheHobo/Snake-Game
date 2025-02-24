e69034b24ce0d52029eaab276b3bbb2a80336243
JoeTheHobo <johnjonesma@gmail.com>
2025-02-23 22:45:37 -0700

    asdf

--- app.js
+++ app.js
@@ -1490,7 +1490,9 @@ function deletePlayer(lobby,player,playerWhoKilled,item,instaKill = false){
 
     if (damage === 0) playerDied = false;
 
+    console.log(player.equiped.head?.whenEquiped?.protect,player.equiped.head?.whenEquiped,player.equiped.head);
     if (player.equiped.head?.whenEquiped?.protect) {
+        console.log("EYY")
         player.equiped.head.whenEquiped.protect -= damage;
         if (player.equiped.head.whenEquiped.protect > 0) {
             playerDied = false;
