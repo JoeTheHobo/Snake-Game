6e2f650f61b46e074a104195a9364a943c87c811
JoeTheHobo <johnjonesma@gmail.com>
2025-02-27 00:40:53 -0700

    asfd

--- public/functions.js
+++ public/functions.js
@@ -1789,7 +1789,7 @@ function loadGameModesToPopup(tab,func) {
         holder.gameMode = gameModes[i];
         holder.on("click",function() {
             $(".chooseGameModePopup").hide()
-            console.log("ey")
+            console.log(func)
             func(this.gameMode);
         })
     }
