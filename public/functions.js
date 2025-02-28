14a6522ab316a659dd8cfc222e3b785b47115baf
JoeTheHobo <johnjonesma@gmail.com>
2025-02-27 19:50:43 -0700

    adsf

--- public/functions.js
+++ public/functions.js
@@ -1587,6 +1587,7 @@ function updateLobbyPage(lobby,type = "all",extra) {
         let reference;
         if (type == "submissionStatus") reference = lobby;
         if (type == "all") reference = lobby.activePlayers;
+        if (type == "players") reference = lobby.activePlayers;
 
         if (reference) {
             for (let i = 0; i < reference.length; i++) {
