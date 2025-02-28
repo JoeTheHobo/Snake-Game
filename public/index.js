a960e90582187135c687886ed4298510943cd3d5
JoeTheHobo <johnjonesma@gmail.com>
2025-02-27 22:24:07 -0700

    asdf

--- public/index.js
+++ public/index.js
@@ -427,7 +427,10 @@ socket.on("updatePositions",(obj,lobbyID) => {
         }
     }
 
-    if (obj.updateSnakeCells) console.log(obj.updateSnakeCells,msgpack.decode(obj.updateSnakeCells));
+    if (obj.updateSnakeCells) {
+        console.log(obj.updateSnakeCells);
+        console.log(obj.updateSnakeCells,msgpack.decode(obj.updateSnakeCells));
+    }
     //if (obj.updateSnakeCells) updateSnakeCells = updateSnakeCells.concat(msgpack.decode(obj.updateSnakeCells));
     if (obj.updateCells) updateCells = updateCells.concat(obj.updateCells);
 
