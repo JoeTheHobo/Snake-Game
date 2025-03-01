1ae5a7df4f9a0cb7549d3bba29e1861f5461f5e8
JoeTheHobo <johnjonesma@gmail.com>
2025-02-28 17:18:00 -0700

    asdf

--- public/functions.js
+++ public/functions.js
@@ -487,7 +487,14 @@ function setScene(scene,lobby) {
     hideScenes();
     $("scene_" + scene).show("flex");
     if (scene == "newMenu") {
-        loadServersHTML();
+        if (items.length > 0 && tiles.length > 0) {
+            loadServersHTML();
+        } else {
+            setTimeout(function() {
+                console.log("waited")
+                loadServersHTML();
+            },100)
+        }
         $(".account_name").innerHTML = localAccount.id; 
         $(".sc_bb_snakeImg").css({
             filter: getPlayerFilter(localAccount.serverSnake),
