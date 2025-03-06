bfb417dfa8c8c404235167bbd37c371b85c98718
JoeTheHobo <johnjonesma@gmail.com>
2025-03-05 19:23:46 -0700

    asdf

--- public/mapEditor.js
+++ public/mapEditor.js
@@ -888,6 +888,7 @@ $("me_button").on("click",function() {
             ],
             {type: "button",close: true, cursor: "url('./img/pointer.cur'), auto", background: "red",text:"Discard Changes",onClick: () => {
                 currentBoard.originalMap = oldMap;
+                saveBoard();
                 setScene("lobby");
             }},
         ],{
