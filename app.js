454723b4b2ef5b3da22c509922c7ab204c068eb5
JoeTheHobo <johnjonesma@gmail.com>
2025-03-05 21:20:38 -0700

    adsf

--- app.js
+++ app.js
@@ -164,8 +164,8 @@ io.on('connection', (socket) => {
             for (let i = 0; i < account.boards.length; i++) {
                 if (account.boards[i].id === board.id) {
                     account.boards[i] = board;
-                    console.log(board.itemDifferences[0][0])
-                    io.emit("updatePlayersBoards",account.boards);
+                    if (board.itemDifferences.length > 0) console.log(board.itemDifferences[0][0])
+                    io.emit("updatePlayersBoards",socket.id,account.boards);
                     compressObject(account.boards,(err,compressed) => {
                         if (err) {
                             console.log(err);
@@ -173,7 +173,7 @@ io.on('connection', (socket) => {
                         }
                         account.boards = compressed;
                         decompressObject(account.boards,(err,decompressed) => {
-                            console.log(decompressed[0].itemDifferences[0][0]);
+                            if (decompressed[0].itemDifferences.length > 0) console.log(decompressed[0].itemDifferences[0][0]);
                         })
                     })
                     return;
