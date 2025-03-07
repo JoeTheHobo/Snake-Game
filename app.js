fae99c64b3e698069694703d272f5035ba33e33e
JoeTheHobo <johnjonesma@gmail.com>
2025-03-06 21:27:13 -0700

    asdf

--- app.js
+++ app.js
@@ -663,7 +663,7 @@ io.on('connection', (socket) => {
             return;
         }
         
-        console.log(lobby.board.originalMap[13][44].onCollision);
+        console.log(lobby.board.originalMap[13][44]);
         lobby.board.map = structuredClone(lobby.board.originalMap);
 
         lobby.oldObj = false;
