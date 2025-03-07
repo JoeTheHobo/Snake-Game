e3fc2bc0484b9db2c5e0b1e37e4e7e535e52965a
JoeTheHobo <johnjonesma@gmail.com>
2025-03-07 16:26:13 -0700

    asdf

--- app.js
+++ app.js
@@ -1822,7 +1822,7 @@ function checkGameMode(gameMode,accountID) {
     if (![false,true].includes(gameMode.respawn)) return "respawn";
     if (![false,true].includes(gameMode.snakeCollision)) return "snakeCollision";
     if (![false,true].includes(gameMode.teamCollision)) return "teamCollision";
-    if (simple.type(gameMode.respawnGrowth) !== "number") return "respawnGrowth";
+    gameMode.respawnGrowth = Number(gameMode.respawnGrowth);
     if (gameMode.respawnGrowth < 0 || gameMode.respawnGrowth > 100) return ["respawnGrowth",gameMode.respawnGrowth];
     if (gameMode.respawnProtection < 0 || gameMode.respawnProtection > 15) return ["respawnProtection",gameMode.respawnProtection];
     if (simple.type(gameMode.respawnTimer) !== "number") return "respawnTimer";
