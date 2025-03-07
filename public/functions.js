29ee5d0a6c02aa52a2a75e11487e76ecc07a008b
JoeTheHobo <johnjonesma@gmail.com>
2025-03-06 21:46:33 -0700

    asdf

--- public/functions.js
+++ public/functions.js
@@ -734,6 +734,7 @@ function findItemDifferences(map) {
             allDifferences.push([differences,j,i]);
         }
     }
+    console.log("CALLED")
     return allDifferences;
 }
 function findTileDifferences(map) {
