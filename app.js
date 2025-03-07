2d67e8241fb6ffdf5541a218c385eae698c669dd
JoeTheHobo <johnjonesma@gmail.com>
2025-03-06 21:35:10 -0700

    asdf

--- app.js
+++ app.js
@@ -1048,28 +1048,27 @@ function fixItemDifferences(lobby,map) {
     let currentBoard = lobby.board;
     if (!currentBoard.itemDifferences) return;
     for (let i = 0; i < currentBoard.itemDifferences.length; i++) {
-        if (e[1] == 44 && e[2] == 13) console.log(1.2,"onCollisionType",simple.type(map[13][44].item.onCollision));
         let e = currentBoard.itemDifferences[i];
         let d = {
             differences: e[0],
             x: e[1],
             y: e[2],
         }
-        if (e[1] == 44 && e[2] == 13) console.log(1.4,"onCollisionType",simple.type(map[13][44].item.onCollision));
+        if (e[1] == 44 && e[2] == 13) console.log(1.2,"onCollisionType",simple.type(map[13][44].item.onCollision));
         let pos = structuredClone(map[d.y][d.x].item);
         if (!pos) continue;
         for (let j = 0; j < d.differences.length; j++) {
             let change = d.differences[j];
             setNestedValue(pos,change,"_LAST_");
         }
-        if (e[1] == 44 && e[2] == 13) console.log(1.6,"onCollisionType",simple.type(map[13][44].item.onCollision));
+        if (e[1] == 44 && e[2] == 13) console.log(1.4,"onCollisionType",simple.type(map[13][44].item.onCollision));
         map[d.y][d.x].item = pos;
         for (let i = 0; i < currentBoard.location_spawns.length; i++) {
             if (d.y == currentBoard.location_spawns[i].y && currentBoard.location_spawns[i].x == d.x) {
                 lobby.board.location_spawns[i].item = map[d.y][d.x].item;
             }
         }
-        if (e[1] == 44 && e[2] == 13) console.log(1.8,"onCollisionType",simple.type(map[13][44].item.onCollision));
+        if (e[1] == 44 && e[2] == 13) console.log(1.6,"onCollisionType",simple.type(map[13][44].item.onCollision));
     }
     console.log(3,"onCollisionType",simple.type(map[13][44].item.onCollision));
 }
