7a6b30edaa7e817c00493d81b7ad27c76694fa36
JoeTheHobo <johnjonesma@gmail.com>
2025-03-15 16:38:37 -0600

    asdf

--- public/mapEditor.js
+++ public/mapEditor.js
@@ -322,20 +322,6 @@ function renderMapEditorCanvas(renderEverything = false) {
         }
     }
     oldMap = structuredClone(currentBoard.originalMap);
-
-    selectedItem.canEdit = true;
-    for (let i = 0; i < items.length; i++) {
-        $("me_item_" + items[i].name).classRemove("me_fullSpawnLimit");
-        if (items[i].spawnLimit === false) {
-            continue;
-        }
-        let count = itemCounts.reduce((acc, item) => (item === ("item_" + items[i].name) ? acc + 1 : acc), 0);
-        if (count === (items[i].spawnLimit*items[i].spawnCount)) {
-            if (selectedItem.cell.name === items[i].name && selectedItem.type == "item") selectedItem.canEdit = false;
-            console.log(items[i]);
-            $("me_item_" + items[i].name).classAdd("me_fullSpawnLimit");
-        }
-    }
 }
 function getRectPos(pos1,pos2) {
     let startX = pos1.x * (gridSize*zoom);
