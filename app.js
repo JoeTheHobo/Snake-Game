09e90bc3d116be33c80a09b3154376e9c373fac6
JoeTheHobo <johnjonesma@gmail.com>
2025-03-16 12:22:51 -0600

    asdf

--- app.js
+++ app.js
@@ -139,6 +139,7 @@ io.on('connection', (socket) => {
     socket.on("saveBoard",(board) => {
         board = fixBoard(JSON.parse(pako.inflate(board, { to: 'string' })));
         if (board.accountID !== socket.id) return;
+        console.log(124,board.itemDifferences)
         //Check Board TO BE ADDED
 
         let account = onlineAccounts[socket.id];
