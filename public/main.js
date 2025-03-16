9d8afe0a1f4c7fab636b270855876bcc59c1da5d
JoeTheHobo <johnjonesma@gmail.com>
2025-03-16 13:35:31 -0600

    asdf

--- public/main.js
+++ public/main.js
@@ -81,8 +81,10 @@ function renderCells() {
 
                 if (mapCell.hideWhen[j].subtract) {
                     if (mapCell.hideWhen[j].subtract[0] === "boardStatusCount") {
-                        console.log(getBaseImgFromTag(mapCell,mapCell.hideWhen[j].subtract[1]));
-                        subtract += localAccount.boardStatus[getBaseImgFromTag(mapCell,mapCell.hideWhen[j].subtract[1])].count;
+                        let gbiftValue = getBaseImgFromTag(mapCell,mapCell.hideWhen[j].subtract[1]);
+                        if (gbiftValue == "white") gbiftValue = localAccount.player.team;
+                        if (gbiftValue !== "white") 
+                            subtract += localAccount.boardStatus[getBaseImgFromTag(mapCell,mapCell.hideWhen[j].subtract[1])].count;
                     }
                 }
 
