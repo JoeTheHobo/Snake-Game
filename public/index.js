c3c24abb5b4c3c774fc2f3b014b02862d18a864a
JoeTheHobo <johnjonesma@gmail.com>
2025-03-16 21:29:57 -0600

    asdf

--- public/index.js
+++ public/index.js
@@ -436,6 +436,7 @@ socket.on("updatePositions",(obj,lobbyID) => {
     }
     if (obj.c) updateCells = updateCells.concat(obj.c);
     if (obj.t) updateTiles = updateTiles.concat(obj.t);
+    console.log(obj.t)
 
     if (obj.p) {
         for (let i = 0; i < obj.p.length; i++) {
--- public/main.js
+++ public/main.js
@@ -845,7 +845,6 @@ function updateProduction() {
 function serverGameLoop() {
     deltaTime = 1;
     if (!isActiveGame) return;
-    console.log(updateTiles)
     renderCells(updateTiles,ctx_tiles)
     renderCells(updateCells,ctx_items);
     //movePlayers();
