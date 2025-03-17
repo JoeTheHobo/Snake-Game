0a5a1c0c07007e39181ff961c7c48d449af790e6
JoeTheHobo <johnjonesma@gmail.com>
2025-03-17 09:42:11 -0600

    adsf

--- public/main.js
+++ public/main.js
@@ -4,7 +4,7 @@ function renderCells(list,ctx) {
         let y = list[i].y;
 
         let mapCell = list[i].item;
-        if (list[i].item.type !== "tile") ctx.clearRect(x*gridSize,y*gridSize,gridSize,gridSize);
+        if (list[i]?.item?.type !== "tile") ctx.clearRect(x*gridSize,y*gridSize,gridSize,gridSize);
         if (!mapCell.visible) continue;
         if (mapCell == false) continue;
         if (mapCell.hideWhen) {
