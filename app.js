bb756182e7e31c9c337e7bb78ccd4c975b553fbb
JoeTheHobo <johnjonesma@gmail.com>
2025-03-16 21:43:56 -0600

    asdf

--- app.js
+++ app.js
@@ -1841,7 +1841,7 @@ function startGameLoop(lobby) {
 
         for (let j = 0; j < object.timeEvents.length; j++) {
             let event = object.timeEvents[j];
-            console.log(evenet)
+            console.log(event)
             if (!event.repeat) event.repeat = 1;
 
             function TimeEvent(event) {
