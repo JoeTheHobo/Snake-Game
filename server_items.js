712d4ed426c27ac7ed1434fc09ffc9788c5ae261
JoeTheHobo <johnjonesma@gmail.com>
2025-03-17 12:02:21 -0600

    asdf

--- server_items.js
+++ server_items.js
@@ -735,7 +735,7 @@ items.push({
     id: 26,
     type: "item",
     baseImg: "items/item_flag_",
-    baseImgTags: [".onCollision.setBoardStatus"],
+    baseImgTags: ["white"],
     renderImages: [["*colors2"]],
     showInEditor: true,
     onStartSpawn: 0,
