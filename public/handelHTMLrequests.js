c904797a5040f681a0dd7837e3a5417cd72d84b3
JoeTheHobo <johnjonesma@gmail.com>
2025-03-20 17:40:03 -0600

    asdf

--- public/handelHTMLrequests.js
+++ public/handelHTMLrequests.js
@@ -248,6 +248,7 @@ $(".sc_bb_customizeSnakeHolder").on("click",function() {
         localAccount.serverSnake.hue = color.hue;
         localAccount.serverSnake.saturation = color.saturation;
         localAccount.serverSnake.brightness = color.brightness;
+        console.log(color)
         savePlayers(localAccount.isInLobby);
     });
 })
