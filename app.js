a73db56d8700f3c4ee712466609c52c09a429331
JoeTheHobo <johnjonesma@gmail.com>
2025-03-20 13:25:57 -0600

    asdf

--- app.js
+++ app.js
@@ -2391,6 +2391,7 @@ function server_movePlayers(lobby,socketID) {
                 if (currentBoard.map[playerY][playerX].item) {
                     let mapItem = currentBoard.map[playerY][playerX].item;
                     if (mapItem.offCollision) runItemFunction(lobby,player,mapItem,"offCollision",{x: playerX,y:playerY});
+                    if (mapTile.id == 9) console.log(mapTile.offCollision,mapTile)
                     if (mapTile.offCollision) {
                         console.log("YEYE")
                         runItemFunction(lobby,player,mapTile,"offCollision",{x: playerX,y:playerY});
