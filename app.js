30805e86ecb8268caef39e68059565d7abf952b6
JoeTheHobo <johnjonesma@gmail.com>
2025-03-20 12:22:09 -0600

    asdf

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ const lobbies = {};
 const onlineAccounts = {};
 
 let newPreset = [];
+/*
 function retrieveAllPresetBoards(index) {
     let buffer = base64ToArrayBuffer(presetBoards[index]);
     decompressObject(buffer,(err,decompressed) => {
@@ -48,6 +49,7 @@ function retrieveAllPresetBoards(index) {
     
 }
 retrieveAllPresetBoards(0);
+*/
 
 io.on('connection', (socket) => {
     console.log('a user connected');    
