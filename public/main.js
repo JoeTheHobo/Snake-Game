271ea618c32d06dabff787a61eadb4bf00a5fdbe
JoeTheHobo <johnjonesma@gmail.com>
2025-03-21 12:06:39 -0600

    adsf

--- public/main.js
+++ public/main.js
@@ -423,7 +423,7 @@ function getPlayerCanvas(holder,image,direction,filter,outline = false,player) {
     let playerCtx = playerCanvas.getContext("2d");
     playerCanvas.width = image.width;
     playerCanvas.height = image.height;
-    if (!outline) playerCtx.filter == filter ? `hue-rotate(${filter}deg)` : getPlayerFilter(player);
+    if (!outline) playerCtx.filter = (filter ? `hue-rotate(${filter}deg)` : getPlayerFilter(player));
 
     if (direction) {
         drawImage(image,direction,0,0,image.width,image.height,playerCanvas);
