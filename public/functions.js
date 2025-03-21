d12d83dbd6573464bce1ae2a49914eddc4525921
JoeTheHobo <johnjonesma@gmail.com>
2025-03-21 14:06:21 -0600

    adsf

--- public/functions.js
+++ public/functions.js
@@ -1799,6 +1799,8 @@ function setAllCanvasToRightSize() {
     let canvasWidth = $(".game_canvas")[0].clientWidth;
     let ratio = width/canvasWidth;
 
+    return;
+
     $(".game_canvas").css({
         transform: `scale(${ratio})`,
     })
