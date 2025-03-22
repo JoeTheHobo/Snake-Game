7f8541907302c568c906d6f80ef78dfee0bd8503
JoeTheHobo <johnjonesma@gmail.com>
2025-03-21 20:53:23 -0600

    asdf

--- public/techTrees.js
+++ public/techTrees.js
@@ -52,7 +52,7 @@ function generateStarBackground(canvas) {
     canvas.width = viewWidth;
     canvas.height = viewHeight;
 
-    const spaceSize = 4000; // Huge space
+    const spaceSize = 50000; // Huge space
     const starCount = Math.floor((spaceSize*10000)/4000); // Number of stars
     const dragSpeed = 0.5;
     const stars = [];
