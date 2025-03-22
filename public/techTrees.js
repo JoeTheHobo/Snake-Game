415356fc77f3828ac36ce6c74c9188a0a6d5208b
JoeTheHobo <johnjonesma@gmail.com>
2025-03-22 15:47:44 -0600

    asdf

--- public/techTrees.js
+++ public/techTrees.js
@@ -155,7 +155,7 @@ function drawLine(parentDiv, childDiv) {
 
     // Create a new line div
     const lineDiv = $("scene_tree").create('div.techTree_line');
-    let lockedImgHolder = lineDiv.create("img.techTree_lineLocked") 
+    let lockedImgHolder = lineDiv.create("div.techTree_lineLocked") 
     let lockedImg = lockedImgHolder.create("img.techTree_lineLockedImg")
     lockedImg.src = "img/menuIcons/lock.png";
 
