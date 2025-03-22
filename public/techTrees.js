606e71d0ecf8c1bdbfb62c9d4612249c406465da
JoeTheHobo <johnjonesma@gmail.com>
2025-03-21 21:17:50 -0600

    asdf

--- public/techTrees.js
+++ public/techTrees.js
@@ -135,11 +135,11 @@ function generateStarBackground(canvas) {
             for (let i = 0; i < movingPoints.length; i++) {
                 movingPoints[i].x -= (e.clientX - startX)*elementDragSpeed;
                 movingPoints[i].y -= (e.clientY - startY)*elementDragSpeed;
-                console.log(movingPoints[i].div.style.left);
+                console.log(movingPoints[i].div.style.left,movingPoints.x);
                 let div = movingPoints[i].div;
                 div.style.left = movingPoints.x + "px";
                 div.style.top = movingPoints.y + "px";
-                console.log(movingPoints[i].div.style.left);
+                console.log(movingPoints[i].div.style.left,movingPoints.x);
             }
 
             startX = e.clientX;
