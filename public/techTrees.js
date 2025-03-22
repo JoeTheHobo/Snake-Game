8368fdbb9161bcf1af600477137485de036f4f57
JoeTheHobo <johnjonesma@gmail.com>
2025-03-22 16:04:38 -0600

    asdf

--- public/techTrees.js
+++ public/techTrees.js
@@ -349,7 +349,7 @@ function generateStarBackground(canvas) {
 
         const clickDuration = Date.now() - mouseDownTime; // Calculate the time between mousedown and mouseup
         if (clickDuration <= maxClickDuration) {
-            if (!e.target.classList.contains("techTree_reward")) {
+            if (!e.target.classList.contains("techTree_reward") && !e.target.classList.contains("techTree_insideReward") && !e.target.classList.contains("techTree_lineLockedImg")) {
                 console.log(e.target)
                 $(".techTree_reward").classRemove("techTree_reward_selected")
                 $(".techTree_infoCard").hide();
