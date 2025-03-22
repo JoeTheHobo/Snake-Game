2587a322e67ea36dae97dc8f430a3a546bab5242
JoeTheHobo <johnjonesma@gmail.com>
2025-03-22 15:06:19 -0600

    asdf

--- public/techTrees.js
+++ public/techTrees.js
@@ -78,7 +78,7 @@ function drawTree(point,comeFromPoint,parentDiv, parentAngle = 0) {
                 let baseAngle = startAngle + i * angleStep + parentAngle;
                 
                 // Generate a deterministic "random" offset using the branch ID
-                let offset = (hashValue(point.id + i) % 2000) / 2000 * 2 - 1; // Value between -1 and 1
+                let offset = (hashValue() % 2000) / 2000 * 2 - 1; // Value between -1 and 1
                 let randomAngle = baseAngle + offset * randomOffset; 
     
                 // Adjust starting edge point
@@ -91,7 +91,7 @@ function drawTree(point,comeFromPoint,parentDiv, parentAngle = 0) {
                 drawTree(point.branches[i], { x: newX, y: newY }, parentDiv, randomAngle);
             }
         } else if (numBranches === 1) {
-            let offset = (hashValue(point.id) % 2000) / 2000 * 2 - 1; 
+            let offset = (hashValue() % 2000) / 2000 * 2 - 1; 
             let randomAngle = parentAngle + offset * randomOffset; 
     
             let newX = ogX + Math.cos(randomAngle) * radius;
@@ -102,6 +102,7 @@ function drawTree(point,comeFromPoint,parentDiv, parentAngle = 0) {
     },150)
 }
 function hashValue(str) {
+    str = rnd("color");
     let hash = 0;
     for (let i = 0; i < str.length; i++) {
         hash = (hash * 31 + str.charCodeAt(i)) % 10000; // Simple deterministic hash
