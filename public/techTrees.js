b9701fd4a0584e1655efebf5df6ef45caae3aec0
JoeTheHobo <johnjonesma@gmail.com>
2025-03-21 20:59:57 -0600

    asdf

--- public/techTrees.js
+++ public/techTrees.js
@@ -2,12 +2,9 @@ function loadTechTree(tree) {
     $("scene_tree").innerHTML = "";
     let canvas = $("scene_tree").create("canvas.techTree_background");
     let vignette = $("scene_tree").create("div.techTree_vignette");
-
     generateStarBackground(canvas);
 
-
     drawTree(tree);
-    
 
     setScene("tree");
 }
