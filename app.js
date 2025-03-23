6946c4d35d65c57db9e006b3ea268e12f2be8af8
JoeTheHobo <johnjonesma@gmail.com>
2025-03-22 18:35:59 -0600

    asdf

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ io.on('connection', (socket) => {
     
     let accessedBattlePasses = {};
     for (let i = 0; i < onlineAccounts[socket.id].battlePasses.length; i++) {
-        accessedBattlePasses[onlineAccounts[socket.id].battlePasses[i].name] = allBattlePasses[onlineAccounts[socket.id].battlePasses[i]];
+        accessedBattlePasses[onlineAccounts[socket.id].battlePasses[i].name] = allBattlePasses[onlineAccounts[socket.id].battlePasses[i].name];
     }
     let randomColor = simple.rnd(onlineAccounts[socket.id].allowedSnakeColors); 
     onlineAccounts[socket.id].serverSnake.hue = randomColor.hue;
