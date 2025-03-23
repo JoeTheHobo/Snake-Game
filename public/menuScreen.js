865837ee45a0b214082bbbc1e566634b9c6a7474
JoeTheHobo <johnjonesma@gmail.com>
2025-03-23 15:44:49 -0600

    asdf

--- public/menuScreen.js
+++ public/menuScreen.js
@@ -1310,5 +1310,8 @@ function loadBoardMenu() {
 }
 
 function loadShopMenu() {
-    
+    $(".menu_tab").classRemove("menu_tab_selected");
+    $(".menu_content").hide();
+    $(".content_shop").show("flex");
+    $("shop_tab").classAdd("menu_tab_selected");
 }
