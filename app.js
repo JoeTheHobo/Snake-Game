53c7ae12e1e7abb074bb6602ae9e435ffcef888b
JoeTheHobo <johnjonesma@gmail.com>
2025-03-22 18:17:21 -0600

    asdf

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const pako = require('pako');
 const profanity = require("./profanity.js");
 
 const allBattlePasses = {};
-const {battlePass_beta} = require("./Tech Trees/techTree_beta.js");
+const battlePass_beta = require("./Tech Trees/techTree_beta.js");
 console.log(battlePass_beta);
 allBattlePasses[battlePass_beta.name] = battlePass_beta;
 
