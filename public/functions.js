b58ae61d9ce60797e72cde8a06ad321a9e37dc21
JoeTheHobo <johnjonesma@gmail.com>
2025-03-23 14:50:05 -0600

    asdf

--- public/functions.js
+++ public/functions.js
@@ -1927,12 +1927,12 @@ function adjustVolume(audioElement, type, adj = 1) {
   })
   $(".lrd_loginButton").on("click",function() {
     let warning = false;
-    let emailValue = $(".lrd_emailInput").input;
+    let emailValue = $(".lrd_emailInput").value;
     let passwordValue = $(".lrd_input_password").value;
 
     if (emailValue == "") warning = "Email Field Is Empty";
     if (passwordValue == "") warning = "Password Field Is Empty";
-    
+
     console.log(emailValue,passwordValue,warning)
 
     if (warning) {
