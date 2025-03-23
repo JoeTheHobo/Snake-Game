a424da2068e399adad50aab4248a68647131b00f
JoeTheHobo <johnjonesma@gmail.com>
2025-03-22 21:38:25 -0600

    adsf

--- public/techTrees.js
+++ public/techTrees.js
@@ -208,7 +208,7 @@ function drawLine(parentDiv, childDiv,unlocked,price,parentUnlocked,nodeID) {
             
             setTimeout(function() {
                 childDiv.activate();
-            },130)
+            },50)
         })
 
     } else {
