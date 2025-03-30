ec18932b87d492d390bf505a6a095bc36e495952
JoeTheHobo <johnjonesma@gmail.com>
2025-03-30 01:30:27 -0600

    asdf

--- app.js
+++ app.js
@@ -622,7 +622,7 @@ io.on('connection', (socket) => {
                         }
                         console.log(boardID,Number(account.tag))
                         let query = "UPDATE boards SET board = ? WHERE id = ? AND tag = ?";
-                        db.query(query,[compressedBoard,boardID,Number(account.tag)],(err)=>{
+                        db.query(query,[compressedBoard,boardID,Number(account.tag)],(err,results)=>{
                             if (err) console.log(64312,err);
                             console.log(results);
                         })
