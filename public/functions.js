8b9d92e829ee0d949c7921e26de951cad9587134
JoeTheHobo <johnjonesma@gmail.com>
2025-03-30 19:30:25 -0600

    asdf

--- public/functions.js
+++ public/functions.js
@@ -778,7 +778,7 @@ function getBoardImage(board,func) {
             }
         }
 
-        let imageData = canvas.toDataURL("image/jpeg",1);
+        let imageData = canvas.toDataURL("image/jpeg",0.9);
         func(imageData)
         canvas.remove(); // Clean up
     }
