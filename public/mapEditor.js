38e9a911988fe89fb8f7c12137e11917bd8637cc
JoeTheHobo <johnjonesma@gmail.com>
2025-04-04 22:10:14 -0600

    asdf

--- public/mapEditor.js
+++ public/mapEditor.js
@@ -1065,6 +1065,8 @@ function saveBoard() {
     html_saveStatus.innerHTML = "Board Saved";
 }
 $("me_button").on("click",function() {
+    socket.emit("db_getAccountBoardStats");
+    
     if (localAccount.isInLobby) {
         makePopUp([
             {type: "title",text: "Save Board"},
