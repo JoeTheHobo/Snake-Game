c88f9ef46ddef874425ad86e5c94087e471b01af
JoeTheHobo <johnjonesma@gmail.com>
2025-04-07 20:41:12 -0600

    asdf

--- public/functions.js
+++ public/functions.js
@@ -2134,7 +2134,7 @@ function showBoardMenu(allBoards) {
         let boardList = searchValue !== "" ? getSimilarNames(searchValue,allBoards[type]) : allBoards[type];
         html_board_content.innerHTML = "";
 
-        console.log(boardList)
+        console.log(allBoards,type)
         if (boardList.length == 0) {
             let noBoards = html_board_content.create("div.cbp_bc_noBoard");
             noBoards.innerHTML = "No Boards Found";
