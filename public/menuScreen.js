c0e06afd73d3950dce2bcf58a68e688f8387245f
JoeTheHobo <johnjonesma@gmail.com>
2025-04-12 19:22:29 -0600

    asdf

--- public/menuScreen.js
+++ public/menuScreen.js
@@ -1192,7 +1192,7 @@ function createGamemodeGrid(holder,width,height,grid) {
     for (let i = 0; i < width; i++) {
         let column = holder.create("div.gmGroup_column");
         column.css({
-            width: (((1/width)*100)-2) + "%",
+            width: (((1/width)*100)-1) + "%",
         })
         columns.push(column);
     }
