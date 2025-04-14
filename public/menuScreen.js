d11ff99ee231cd153756b23256b9c513eed9f383
JoeTheHobo <johnjonesma@gmail.com>
2025-04-13 21:11:10 -0600

    asdf

--- public/menuScreen.js
+++ public/menuScreen.js
@@ -1590,8 +1590,9 @@ function loadGamemodeTabWinning() {
         }
 
         let a = createGamemodeSetting("Who Wins","list","whoWins",{options: ["Player","Players Team","Specific Team"],caseSensitive: true},"When winning condition is met who wins?",false,false,false,(value) => {
-            if (value !== "Specific Team") return;
-            
+            console.log(value)
+            if (value.toLowerCase() !== "specific team") return;
+
             showStatusMenu(["status"],{status: function(status) {
                 gamemode.winningConditions[index].whoWins = status;
                 $(".statusSelectionScreen").hide();
