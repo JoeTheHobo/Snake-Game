0bdb43f824f75dec27fa8730c454573bddb30e23
JoeTheHobo <johnjonesma@gmail.com>
2025-04-13 21:22:28 -0600

    asdf

--- public/menuScreen.js
+++ public/menuScreen.js
@@ -1378,8 +1378,7 @@ function generateGamemodeSetting(holder,settings,pullFrom) {
     if (settings.type == "item" || settings.type == "tile") {
         valueInput = holder.create("div.gmGroup_imageHolder");
         let img = valueInput.create("img.fullImage");
-        console.log(getNestedValue(pullFrom,settings.valueString));
-        img.src = getImage(getByID(Number(getNestedValue(pullFrom,settings.valueString)),settings.type),"src")
+        img.src = getImage(getById(settings.type,Number(getNestedValue(pullFrom,settings.valueString))),"src")
         holder.gmValue = getNestedValue(pullFrom,settings.valueString);
 
 
