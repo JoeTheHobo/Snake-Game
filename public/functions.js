2a556a0bcfbef82109173f422bc5809605603ada
JoeTheHobo <johnjonesma@gmail.com>
2025-04-13 22:27:16 -0600

    asdf

--- public/functions.js
+++ public/functions.js
@@ -1507,8 +1507,7 @@ function logAllWinningConditions(holder,gamemode) {
         let howToWin;
 
         if (whoWins !== "Players Team" && whoWins !== "Player") {
-            console.log(whoWins,`img/items/item_flag_basic_${whoWins}.png`)
-            whoWins =  `<img class="gm_alt_team" src="img/items/item_flag_basic_${whoWins}.png> Team ${whoWins}`;
+            whoWins =  `<img class="gm_alt_team" src="img/items/item_flag_basic_${whoWins}.png"> Team ${whoWins}`;
         }
 
         if (condition.condition == "Last One Standing") {
