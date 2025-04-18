6e04ac9cf7e78f10e9bf1ef5a06ef293696ac587
JoeTheHobo <johnjonesma@gmail.com>
2025-04-17 21:34:11 -0600

    asdf

--- public/db.js
+++ public/db.js
@@ -449,7 +449,7 @@ function drawNodeImage(ctx,image,x,y,nodeSize,nodeIndex,element) {
             let shrinkNodeSize = nodeSize * shrinkSize;
             let shrinkDifference = nodeSize - shrinkNodeSize;
 
-            ctx.drawImage(srcImage, x, y, shrinkNodeSize, shrinkNodeSize);
+            ctx.drawImage(srcImage, x+(shrinkDifference/2), y+(shrinkDifference/2), shrinkNodeSize, shrinkNodeSize);
 
         }
     }
