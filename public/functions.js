381b35fb6b791a9f8a9df97c55d718ea0d9480f7
JoeTheHobo <johnjonesma@gmail.com>
2025-04-19 01:12:55 -0600

    asdf

--- public/functions.js
+++ public/functions.js
@@ -1515,7 +1515,7 @@ function logAllWinningConditions(holder,gamemode) {
         let whoWins = condition.whoWins;
         let howToWin;
 
-        if (whoWins !== "Players Team" && whoWins !== "Player" && whoWins !== "Everyone" && whoWins !== "Highest Value") {
+        if (whoWins !== "Players Team" && whoWins !== "Player" && whoWins !== "Everyone" && whoWins !== "Highest Value" && whoWins !== "No One") {
             whoWins =  `<img class="gm_alt_team" src="img/items/item_flag_basic_${whoWins}.png"> ${whoWins.format("A")} Team`;
         }
         if (whoWins == "Everyone") {
