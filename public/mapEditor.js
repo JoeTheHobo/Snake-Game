6e9da55df65d10c99e344f5dc5f1b2eeba1a3dc5
JoeTheHobo <johnjonesma@gmail.com>
2025-04-19 14:16:48 -0600

    asdf

--- public/mapEditor.js
+++ public/mapEditor.js
@@ -1572,8 +1572,7 @@ function runTool(type,desiredValue) {
         if (showFullGrid) $(".show_grid_tool2").classAdd("toolIsSelected");
         else $(".show_grid_tool2").classRemove("toolIsSelected");
     }
-    console.log(type.subset(0,"_\\before"));
-    if (type.subset(0,"_\\before") == "show_zone") {
+    if (type.subset(0,8) == "show_zone") {
         let zoneType = type.subset("_\\after","end");
         if (zoneType == "player") userClickedZonePlayer = !userClickedZonePlayer;
         if (zoneType == "item") userClickedZoneItem = !userClickedZoneItem;
