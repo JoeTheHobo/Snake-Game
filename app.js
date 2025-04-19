f5780fc6c97a6d1f4c6754b1734748de3998ab26
JoeTheHobo <johnjonesma@gmail.com>
2025-04-19 17:14:37 -0600

    asdf

--- app.js
+++ app.js
@@ -2841,7 +2841,7 @@ function specialZone_testOccupied(lobby,zone) {
 function specialZone_onEnter(lobby,zone) {
     console.log(1)
     if (!zone.giveStatusOnEnter) return;
-    console.log(2)
+    console.log(2,zone.repeatStatusType,zone.statusGave)
     if (zone.repeatStatusType == "Single Use" && zone.statusGave > 0) return;
     console.log(3)
     let correctOccupied = specialZone_testOccupied(lobby,zone);
