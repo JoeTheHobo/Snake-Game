9527f945f115b596efcdec4b29aba19fed0fb072
JoeTheHobo <johnjonesma@gmail.com>
2025-04-18 23:11:37 -0600

    asdf

--- public/functions.js
+++ public/functions.js
@@ -1569,6 +1569,7 @@ function logGameModeChanges(holder,gameMode) {
 
     for (let i = 0; i < useArr.length; i++) {
         let key = useArr[i];
+        if (gameMode[key] === undefined) gameMode[key] = basedGameMode[key];
         if (basedGameMode[key] !== gameMode[key]) alterations.push({
             key: formatString(key),
             oldValue: basedGameMode[key],
