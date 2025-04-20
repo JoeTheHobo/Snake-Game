ea52ec5907cc8b32b513146c9de4b0bcab33e3ba
JoeTheHobo <johnjonesma@gmail.com>
2025-04-20 11:48:17 -0600

    as

--- public/db.js
+++ public/db.js
@@ -434,7 +434,7 @@ function pass_drawSet(canvas, i, set) {
 
                 ctx.save();
                 ctx.translate(fromX, fromY);
-                ctx.rotate(angle + Math.PI / 2);
+                ctx.rotate(angle);
 
                 const arrowWidth = 20;
                 const arrowHeight = 10;
