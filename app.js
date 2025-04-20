e38921c48a7d2b51d7c3ad744e1e573fedaf8983
JoeTheHobo <johnjonesma@gmail.com>
2025-04-20 12:15:13 -0600

    asdf

--- app.js
+++ app.js
@@ -108,6 +108,7 @@ const onlineAccounts = {};
 io.on('connection', (socket) => { 
     socket.join(socket.id);
     socket.join("menuScreen");
+    socket.emit(socket.id);
     setGuestAccount(socket.id,true);
 
     //socket.emit communicates with the player that just connected, io.emit communicates with the whole lobby
