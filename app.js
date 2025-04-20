6f56a6f7dcfacc859d35290f6806dcdf488cb0ab
JoeTheHobo <johnjonesma@gmail.com>
2025-04-20 12:08:04 -0600

    adsf

--- app.js
+++ app.js
@@ -1777,6 +1777,7 @@ io.on('connection', (socket) => {
                         completed++;
                         if (completed === tableNames.length) {
                             // Step 4: Send result back
+                            console.log("EYOWAZZUP")
                             io.to(socket.id).emit("adminTools_giveDatabaseData", {
                                 columnNames: Array.from(allColumns),
                                 tableNames: tableNames,
