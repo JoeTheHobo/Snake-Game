69813c856b7327356132a6241cacbae0fc9e26f0
JoeTheHobo <johnjonesma@gmail.com>
2025-04-22 22:11:12 -0600

    asdf

--- public/functions.js
+++ public/functions.js
@@ -2661,6 +2661,7 @@ function renderZone_color(zone,value,settings) {
     if (colorResults.toLowerCase() == "none") colorResults = "#00000000";
 
     let opacity = Number(settings.opacity) || 1;
+    console.log(settings.opacity)
     return _color(colorResults,opacity).color;
 }
 function renderZone_checkForValue(zone,value) {
