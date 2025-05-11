1bd2f8dcfe905dcb97a02576d4257546f99afb27
JoeTheHobo <johnjonesma@gmail.com>
2025-05-10 18:45:34 -0600

    asdf

--- app.js
+++ app.js
@@ -267,6 +267,7 @@ io.on('connection', (socket) => {
     })
     socket.on("signInUsingToken",(token,email) => {
         const query = "SELECT * FROM credentials WHERE email = ?";
+        console.log("EYO")
         db.query(query,[email],(err,results) => {
             if (err) {
                 console.log(1452,err);
