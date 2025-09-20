a14c87e61e6f6b60c762e8975fa7c3412d3869dc
JoeTheHobo <johnjonesma@gmail.com>
2025-09-20 17:42:08 -0600

    asdf

--- public/index.js
+++ public/index.js
@@ -752,7 +752,7 @@ function handelRespawnTimer(time,deathPoint) {
         return;
     }
 
-    let seconds = time-Math.round(miliseconds/1000);
+    let seconds = time-Math.ceil(miliseconds/1000);
     $(".whiteTextAtTopOfGame").innerHTML = `Respawn In ${seconds} Seconds`;
 
 
