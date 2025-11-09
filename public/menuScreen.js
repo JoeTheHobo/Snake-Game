097a58adebe798fc315c28f1293552ab55cd3d52
JoeTheHobo <johnjonesma@gmail.com>
2025-11-09 11:31:05 -0700

    asdf

--- public/menuScreen.js
+++ public/menuScreen.js
@@ -395,9 +395,9 @@ socket.on("returningUserStats",(stats) => {
     let correctStatList = [];
     for (let i = 0; i < stats.length; i++) {
         let stat = stats[i];
-        if (stat.stat_name.includes("item") || stat.stat_name.includes("title")) {
+        if (stat.stat_name.includes("item") || stat.stat_name.includes("tile")) {
             let type = stat.stat_name.format("<").subset("_\\after",true).subset(0,"_\\before").format("<");
-            let id = Number(stat.stat_name.format("<").subset(0,"_\\before"));
+            let id = Number(stat.stat_name.format("<").subset(0,"_\\before").format("<"));
             let item = getById(type,id);
             console.log(id)
             let displayName = item?.displayName;
