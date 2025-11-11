f0beb92b119e97ca11e45f81d689c79b4e1ec6b1
JoeTheHobo <johnjonesma@gmail.com>
2025-11-11 00:05:46 -0700

    asdf

--- public/functions.js
+++ public/functions.js
@@ -2885,9 +2885,8 @@ function generateObjectiveCard(card,challenge,type) {
         completed false
         star "one"
     */
-   console.log(challenge)
 
-    let displayText = card.create("div.obj_displayText>" + card.display_text);
+    let displayText = card.create("div.obj_displayText>" + challenge.display_text);
 
     //Create Stars
     let starContainer = card.create("div.obj_starContainer");
