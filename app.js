91dd24584df2060bac35a686b2a6c2ba9e7e56ab
JoeTheHobo <johnjonesma@gmail.com>
2025-11-11 19:13:21 -0700

    asdf

--- app.js
+++ app.js
@@ -4243,8 +4243,8 @@ function setSocketToUser(account,user,dbObj) {
             account.activeChallenge2 = null;
             account.activeChallenge3 = null;
             db.query(
-                "UPDATE inventory SET weekly_challenges = ? active_challenges WHERE tag = ?",
-                [JSON.stringify(account.weeklyChallenges),JSON.stringify(account.active_challenges), user.tag]
+                "UPDATE inventory SET weekly_challenges = ?, active_challenge_1 = null, active_challenge_2 = null, active_challenge_3 = null, WHERE tag = ?",
+                [JSON.stringify(account.weeklyChallenges), user.tag]
             );
 
         }
