3f060d87ee05543bd6d8ad759d385d5ef44fa100
JoeTheHobo <johnjonesma@gmail.com>
2025-11-11 18:38:46 -0700

    asdf

--- public/functions.js
+++ public/functions.js
@@ -2885,7 +2885,7 @@ function generateObjectiveCard(card,challenge,type) {
         completed false
         star "one"
     */
-
+    console.log(card)
     if (card === null) {
         let displayText = card.create("div.obj_displayText>No Objective Selected");
         return;
